c44e3515712399a1743e80b6810fe4dd98bda6f2
bhsingleton <bhsingleton@gmail.com>
2021-08-10 16:39:38 -0400

    Adding function to restart package.

---
+++ __init__.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+def restart():
+    """
+    Removes all modules, derived from the Robot Entertainment package, from the system module.
+    This will force any future imported modules to recompile.
+
+    :rtype: None
+    """
+
+    # Iterate through modules
+    # Use this package to differentiate modules
+    #
+    directory = os.path.dirname(os.path.abspath(__file__))
+    moduleNames = set(sys.modules.keys())
+
+    for moduleName in moduleNames:
+
+        # Check if module has file attribute
+        #
+        module = sys.modules[moduleName]
+
+        if not hasattr(module, '__file__'):
+
+            continue
+
+        # Check for none type
+        #
+        if module.__file__ is None:
+
+            continue
+
+        # Check if module falls under python paths
+        #
+        filePath = os.path.abspath(module.__file__)
+
+        if filePath.startswith(directory):
+
+            log.info('Rolling back module: %s' % module)
+            del sys.modules[moduleName]
