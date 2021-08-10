d2fc7e82427c04f311019cb9aac26559d51464a9
bhsingleton <bhsingleton@gmail.com>
2021-08-10 16:41:03 -0400

    Adding inclusive range generator.

---
+++ generators/inclusiverange.py
@@ -0,0 +1,51 @@
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+def inclusiveRange(*args, **kwargs):
+    """
+    Generator method used to yield an inclusive range for use with plug elements.
+
+    :rtype: iter
+    """
+
+    # Inspect supplied arguments
+    #
+    start = None
+    end = None
+    step = None
+
+    numArgs = len(args)
+
+    if numArgs == 1:
+
+        start = 0
+        end = args[0]
+        step = 1 if start <= end else -1
+
+    elif numArgs == 2:
+
+        start = args[0]
+        end = args[1]
+        step = 1 if start <= end else -1
+
+    elif numArgs == 3:
+
+        start = args[0]
+        end = args[1]
+        step = args[2]
+
+    else:
+
+        raise TypeError('inclusiveRange() expects 1 to 3 argument(s) (%s given)!' % numArgs)
+
+    # Yield integer values
+    #
+    i = start
+
+    while i <= end:
+
+        yield i
+        i += step
