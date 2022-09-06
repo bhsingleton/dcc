e3d61879dbab6ecb5c153317f589fdd9e9ad2c8f
bhsingleton <bhsingleton@gmail.com>
2022-09-06 14:32:21 -0400

    Adding remoeMentalRayPlugin method.

--- maya/libs/sceneutils.py
+++ maya/libs/sceneutils.py
@@ -91,25 +91,41 @@ def removeUserAttributes():
             mc.deleteAttr('%s.%s' % (nodeName, attrName))
 
 
-def unloadTurtlePlugin():
+def removeUnknownNodes():
     """
-    Unloads the turtle plugin from the open scene file.
+    Removes any unknown nodes from the scene file.
 
+    :rtype: None
+    """
+
+    nodeNames = mc.ls(type='unknown')
+
+    if nodeNames is not None:
+
+        log.info('Deleting unknown nodes: %s' % nodeNames)
+        mc.delete(nodeNames)
+
+
+def unloadPlugin(pluginName):
+    """
+    Unloads the specified plugin and removes any associated nodes.
+
+    :type pluginName: str
     :rtype: None
     """
 
     # Check if turtle is loaded
     #
-    isLoaded = mc.pluginInfo('Turtle', query=True, loaded=True)
+    isLoaded = mc.pluginInfo(pluginName, query=True, loaded=True)
 
     if not isLoaded:
 
-        log.info('Could not locate "Turtle" in the open scene file.')
+        log.info('Could not locate "%s" plugin.')
         return
 
-    # Remove all node types associated with turtle
+    # Remove all node types associated with plugin
     #
-    nodeTypes = mc.pluginInfo('Turtle', query=True, dependNode=True)
+    nodeTypes = mc.pluginInfo(pluginName, query=True, dependNode=True)
 
     for nodeType in nodeTypes:
 
@@ -128,8 +144,25 @@ def unloadTurtlePlugin():
         mc.delete(nodeNames)
 
     # Flush undo queue
+    # Otherwise this will prevent the plugin from unloading!
     #
     mc.flushUndo()
+
+    # Unlock plugin
+    #
+    mc.unloadPlugin(pluginName)
+
+
+def unloadTurtlePlugin():
+    """
+    Unloads the turtle plugin from the open scene file.
+
+    :rtype: None
+    """
+
+    # Unload turtle plugin
+    #
+    unloadPlugin('Turtle')
 
     # Remove shelf from tab bar
     #
@@ -138,9 +171,27 @@ def unloadTurtlePlugin():
         log.info('Removing "TURTLE" from the shelf tab!')
         mc.deleteUI('TURTLE', layout=True)
 
-    # Unlock plugin
+
+def unloadMentalRayPlugin():
+    """
+    Unloads the mental-ray plugin from the open scene file.
+    If the plugin no longer exists then unknown nodes are removed.
+
+    :rtype: None
+    """
+
+    # Check if mental-ray is loaded
     #
-    mc.unloadPlugin('Turtle')
+    pluginName = 'mayatomr'
+    isLoaded = mc.pluginInfo(pluginName, query=True, loaded=True)
+
+    if isLoaded:
+
+        unloadPlugin(pluginName)
+
+    else:
+
+        removeUnknownNodes()
 
 
 def resetWindowPositions():
