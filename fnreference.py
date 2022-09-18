092e3ffaa567ae0f984239dbe1203bc8effc5f6c
bhsingleton <bhsingleton@gmail.com>
2022-09-18 09:53:35 -0400

    Providing developers the ability to override the modules function set.

--- fnreference.py
+++ fnreference.py
@@ -1,4 +1,7 @@
+import inspect
+
 from . import __application__
+from .abstract import afnreference
 
 import logging
 logging.basicConfig()
@@ -17,3 +20,29 @@ elif __application__ == '3dsmax':
 else:
 
     raise ImportError('Unable to import dcc reference-helpers for: %s application!' % __application__)
+
+
+def overrideFunctionSet(cls):
+    """
+    Overrides the imported DCC function set.
+    This is important for DCC applications that require custom reference systems such as 3ds Max.
+
+    :type cls: class
+    :rtype: None
+    """
+
+    # Verify this is a class
+    #
+    if not inspect.isclass(cls):
+
+        return overrideFunctionSet(type(cls))
+
+    # Verify this is a subclass of AFnReference
+    #
+    if issubclass(cls, afnreference.AFnReference):
+
+        __dict__['FnReference'] = cls
+
+    else:
+
+        raise TypeError('overrideFunctionSet() expects an AFnReference subclass (%s given)!' % cls.__name__)
