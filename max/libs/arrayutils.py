8a2e8d01682d5ba5fd6f7898b208d85e0bf8b60a
bhsingleton <bhsingleton@gmail.com>
2022-09-18 17:58:53 -0400

    Optimizing iterBitArray generator.

--- max/libs/arrayutils.py
+++ max/libs/arrayutils.py
@@ -6,6 +6,20 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
+__convert_bitarray__ = pymxs.runtime.execute('fn bitArrayToArray bits = ( bits as array );')
+
+
+def convertBitArray(bits):
+    """
+    Returns an array of elements from the supplied bit array.
+
+    :type bits: pymxs.runtime.BitArray
+    :rtype: pymxs.runtime.Array
+    """
+
+    return __convert_bitarray__(bits)
+
+
 def iterElements(array):
     """
     Returns a generator that yields elements from an array.
@@ -22,18 +36,9 @@ def iterBitArray(bits):
     """
     Returns a generator that yields the elements from a bit array.
 
-    :type bits: pymxs.runtime.MXSWrapperBase
+    :type bits: pymxs.runtime.BitArray
     :rtype: iter
     """
 
-    for i in range(bits.count):
-
-        bit = bits[i]
-
-        if bit:
-
-            yield i + 1
-
-        else:
-
-            continue
+    elements = convertBitArray(bits)
+    return iterElements(elements)
