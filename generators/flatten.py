fd3b4b7419c14ed37600b2ca8cc1ce5e4e0d8688
bhsingleton <bhsingleton@gmail.com>
2022-09-18 09:44:26 -0400

    Adding flatten generator.

---
+++ generators/flatten.py
@@ -0,0 +1,31 @@
+import collections
+
+from six import string_types
+from six.moves import collections_abc
+from collections import deque
+
+
+def flatten(*items):
+    """
+    Returns a generator that flattens the supplied items and yields them.
+
+    :rtype: Iterator[Any]
+    """
+
+    queue = deque(items)
+
+    while len(queue) > 0:
+
+        item = queue.popleft()
+
+        if isinstance(item, collections_abc.Sequence) and not isinstance(item, string_types):
+
+            queue.extendleft(reversed(item))
+
+        elif isinstance(item, collections.Iterable) and not isinstance(item, string_types):
+
+            queue.extendleft(reversed(list(item)))
+
+        else:
+
+            yield item
