efbd2ffda3d2e55eb10c251ed512cf7bfd12e605
bhsingleton <bhsingleton@gmail.com>
2022-09-28 23:11:40 -0400

    Adding programmaticallyChanged signal.

--- ui/qdirectoryedit.py
+++ ui/qdirectoryedit.py
@@ -1,6 +1,4 @@
-import os
-
-from Qt import QtCore, QtWidgets, QtGui, QtCompat
+from Qt import QtCore, QtWidgets, QtGui
 from ..python import stringutils
 
 import logging
@@ -13,6 +11,10 @@ class QDirectoryEdit(QtWidgets.QLineEdit):
     """
     Overload of QLineEdit that includes a directory explorer button.
     """
+
+    # region Signals
+    programmaticallyChanged = QtCore.Signal(str)
+    # endregion
 
     # region Dunderscores
     def __init__(self, *args, **kwargs):
@@ -31,13 +33,13 @@ class QDirectoryEdit(QtWidgets.QLineEdit):
 
         # Declare private variables
         #
-        self._requiresUserInput = False
         self._caption = kwargs.get('caption', 'Select Directory')
-        self._dir = kwargs.get('dir', os.getcwd())
+        self._requiresUserInput = False
 
         # Add custom action
         #
         action = QtWidgets.QAction(QtGui.QIcon(':dcc/icons/open_folder.svg'), '', parent=self)
+        action.setToolTip('Select directory from dialog.')
         action.triggered.connect(self.on_action_triggered)
 
         self.addAction(action, QtWidgets.QLineEdit.TrailingPosition)
@@ -46,12 +48,13 @@ class QDirectoryEdit(QtWidgets.QLineEdit):
     # region Methods
     def text(self):
         """
-        Returns the text from the line edit.
+        Returns the line edit's text.
 
         :rtype: str
         """
 
         # Check if user input is required
+        # This is done for sake of circumventing crashes from a QStyledItemDelegate
         #
         if self._requiresUserInput:
 
@@ -65,6 +68,19 @@ class QDirectoryEdit(QtWidgets.QLineEdit):
         # Call parent method
         #
         return super(QDirectoryEdit, self).text()
+
+    def setText(self, text):
+        """
+        Updates the line edit's text.
+
+        :type text: str
+        :rtype: None
+        """
+
+        # Call parent method
+        #
+        super(QDirectoryEdit, self).setText(text)
+        self.programmaticallyChanged.emit(text)
 
     def isEditor(self):
         """
@@ -88,7 +104,7 @@ class QDirectoryEdit(QtWidgets.QLineEdit):
         return QtWidgets.QFileDialog.getExistingDirectory(
             parent=self,
             caption=self._caption,
-            dir=self._dir,
+            dir=self.text(),
             options=QtWidgets.QFileDialog.ShowDirsOnly
         )
     # endregion
