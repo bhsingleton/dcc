e2c920a986aeb1fb714152b24d6f0693a00476c9
Ben Singleton <bhsingleton@gmail.com>
2023-07-05 10:39:34 -0400

    Splitting functions into separate likeness comparison functions.

--- python/arrayutils.py
+++ python/arrayutils.py
@@ -1,9 +1,21 @@
 from six import string_types
+from six.moves import collections_abc
 
 import logging
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
+
+
+def isArray(obj):
+    """
+    Evaluates if the supplied object is an array.
+
+    :type obj: Any
+    :rtype: bool
+    """
+
+    return isinstance(obj, collections_abc.Sequence) and not isinstance(obj, string_types)
 
 
 def isArrayLike(obj):
@@ -18,12 +30,23 @@ def isArrayLike(obj):
     return (hasattr(obj, '__getitem__') and hasattr(obj, '__len__')) and not isinstance(obj, string_types)
 
 
-def isIterable(obj):
+def isIterator(obj):
     """
-    Evaluates if the supplied object is iterable.
+    Evaluates if the supplied object is an iterator.
 
     :type obj: Any
     :rtype: bool
     """
 
-    return hasattr(obj, '__iter__') and hasattr(obj, '__next__')
+    return isinstance(obj, collections_abc.Iterator) and not isinstance(obj, string_types)
+
+
+def isIteratorLike(obj):
+    """
+    Evaluates if the supplied object is like an iterator.
+
+    :type obj: Any
+    :rtype: bool
+    """
+
+    return (hasattr(obj, '__iter__') and hasattr(obj, '__next__')) and not isinstance(obj, string_types)
