937bcf11885d2631a603428f9bd4b064b975ee8f
Ben Singleton <bhsingleton@gmail.com>
2024-03-07 08:29:09 -0500

    Adding `createCurveFromPoints` function.

--- maya/libs/shapeutils.py
+++ maya/libs/shapeutils.py
@@ -1,10 +1,11 @@
 import os
+import math
 import json
 
 from maya import cmds as mc, OpenMaya as lom
 from maya.api import OpenMaya as om
 from enum import IntEnum
-from . import dagutils
+from . import dagutils, plugutils
 from ..json import mshapeparser
 from ...python import stringutils
 
@@ -15,6 +16,9 @@ log.setLevel(logging.INFO)
 
 
 class ObjectColor(IntEnum):
+    """
+    Collection of all available object colours.
+    """
 
     ORANGE = 0
     YELLOW = 1
@@ -138,6 +142,195 @@ def colorizeShape(*shapes, **kwargs):
             continue
 
 
+def createKnotVector(count, degree, periodic=False):
+    """
+    Returns a knot vector from the given parameters.
+    The minimum number of CVs needed to create a periodic curve is: numCVs >= ( 2 * degree ) + 1
+    But all periodic curves must have: numCVs == ( spans + degree )
+
+    :type count: int
+    :type degree: int
+    :type periodic: bool
+    :rtype: Tuple[List[int], int]
+    """
+
+    if periodic:
+
+        # Check if there are enough CVs
+        #
+        minimum = (2 * degree) + 1
+
+        if count == minimum:
+
+            degree -= 1
+
+        elif count < minimum:
+
+            raise TypeError('createKnotVector() expects at least %s CVs (%s given)!' % (minimum, count))
+
+        else:
+
+            pass
+
+        # Generate knots
+        #
+        numSpans = count - degree
+        numKnots = numSpans + (2 * degree) - 1
+        knots = [x for x in range(-(degree - 1), (numKnots - (degree - 1)), 1)]
+
+        return knots, degree
+
+    else:
+
+        # Check if there are enough CVs
+        # numKnots = count + degree - 1
+        #
+        if count == degree:
+
+            degree -= 1
+
+        elif count < degree:
+
+            raise TypeError(f'createKnotVector() expects at least {degree} CVs ({count} given)!')
+
+        else:
+
+            pass
+
+        # Generate knots
+        #
+        knotCount = count + degree - 1
+        headSize = degree
+        tailSize = knotCount - degree
+        tailValue = (float(knotCount) - (degree * 2.0)) + 1.0
+
+        knots = [0] * knotCount
+
+        for i in range(knotCount):
+
+            if 0 <= i < headSize:
+
+                knots[i] = 0.0
+
+            elif headSize <= i < tailSize:
+
+                knots[i] = float(i) - (headSize - 1.0)
+
+            else:
+
+                knots[i] = tailValue
+
+        return knots, degree
+
+
+def createCurveFromPoints(controlPoints, degree=1, periodic=False, parent=om.MObject.kNullObj):
+    """
+    Creates a curve data object from the supplied points.
+
+    :type controlPoints: List[om.MVector]
+    :type degree: int
+    :type periodic: bool
+    :type parent: om.MObject
+    :rtype: om.MObject
+    """
+
+    numControlPoints = len(controlPoints)
+    form = om.MFnNurbsCurve.kPeriodic if periodic else om.MFnNurbsCurve.kOpen
+    knots, degree = createKnotVector(numControlPoints, degree, periodic=periodic)
+
+    hasParent = not parent.isNull()
+
+    if hasParent:
+
+        fnCurve = om.MFnNurbsCurve()
+        curve = fnCurve.create(controlPoints, knots, degree, form, False, True, parent=parent)
+
+        curveName = f'{dagutils.getNodeName(parent)}Shape'
+        fnCurve.setName(curveName)
+
+        return curve
+
+    else:
+
+        fnCurveData = om.MFnNurbsCurveData()
+        curveData = fnCurveData.create()
+
+        fnCurve = om.MFnNurbsCurve()
+        fnCurve.create(controlPoints, knots, degree, form, False, True, parent=curveData)
+
+        return curveData
+
+
+def createStar(outerRadius, innerRadius, numPoints=5, normal=om.MVector.kXaxisVector, parent=om.MObject.kNullObj):
+    """
+    Creates a star nurbs curve data object.
+    If a parent is supplied then a new shape is created under that object.
+
+    :type outerRadius: float
+    :type innerRadius: float
+    :type numPoints: int
+    :type normal: om.MVector
+    :type parent: om.MObject
+    :rtype: om.MObject
+    """
+
+    # Collect star points
+    #
+    size = numPoints * 2
+    angleFactor = (math.pi * 2.0) / size
+
+    controlPoints = om.MPointArray()
+
+    for i in range(size):  # Skip last point since it's a close curve!
+
+        # Compute point
+        #
+        isEven = (i % 2) == 0
+        angle = float(i) * angleFactor
+
+        x, y = 0.0, 0.0
+
+        if isEven:
+
+            x, y = math.cos(angle) * outerRadius, math.sin(angle) * outerRadius
+
+        else:
+
+            x, y = math.cos(angle) * innerRadius, math.sin(angle) * innerRadius
+
+        # Rotate point into normal space
+        #
+        quat = om.MVector.kZaxisVector.rotateTo(normal)
+        point = om.MVector(x, y, 0.0).rotateBy(quat)
+
+        controlPoints.append(om.MPoint(point))
+
+    # Check if a parent was supplied
+    #
+    knots, degree = createKnotVector(len(controlPoints), 1, periodic=True)
+    hasParent = not parent.isNull()
+
+    if hasParent:
+
+        fnCurve = om.MFnNurbsCurve()
+        curve = fnCurve.create(controlPoints, knots, degree, om.MFnNurbsCurve.kClosed, False, True, parent=parent)
+
+        curveName = f'{dagutils.getNodeName(parent)}Shape'
+        fnCurve.setName(curveName)
+
+        return curve
+
+    else:
+
+        fnCurveData = om.MFnNurbsCurveData()
+        curveData = fnCurveData.create()
+
+        fnCurve = om.MFnNurbsCurve()
+        fnCurve.create(controlPoints, knots, degree, om.MFnNurbsCurve.kClosed, False, True, parent=curveData)
+
+        return curveData
+
+
 def createShapeTemplate(node, filePath):
     """
     Creates a shape template from the supplied transform or shape node.
@@ -150,15 +343,15 @@ def createShapeTemplate(node, filePath):
     # Evaluate api type
     #
     node = dagutils.getMObject(node)
-    shapes = []
+    shapes = None
 
     if node.hasFn(om.MFn.kTransform):
 
-        shapes.extend(list(dagutils.iterShapes(node)))
+        shapes = list(dagutils.iterShapes(node))
 
     elif node.hasFn(om.MFn.kShape):
 
-        shapes.append(node)
+        shapes = [node]
 
     else:
 
